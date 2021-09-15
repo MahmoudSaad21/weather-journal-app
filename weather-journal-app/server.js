e74c5d7280208edde21778f584092044e70cc215
MahmoudSaad21 <86734676+MahmoudSaad21@users.noreply.github.com>
2021-09-15 19:12:49 +0200

    Add files via upload

---
+++ weather-journal-app/server.js
@@ -0,0 +1,40 @@
+// Setup empty JS object to act as endpoint for all routes
+projectData = {};
+
+// Require Express to run server and routes
+const express = require('express');
+// Start up an instance of app
+
+const app = express();
+const bodyParser = require('body-parser')
+
+
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+// Initialize the main project folder
+app.use(express.static('website'));
+const port = 3030;
+
+// Setup Server
+const server = app.listen(port, () => {
+    console.log(`running on localhost: ${port}`)
+});
+f
+app.get("/getInformations", getInformations);
+app.post("/saveInformations", saveInformations)
+    //functionto get date
+
+function getInformations(req, res) {
+    res.send(projectData);
+}
+//function to save data in the enpoint (projectData)
+
+function saveInformations(req, res) {
+    projectData = {...req.body }
+    res.end();
+}
---
+++ weather-journal-app/website/app.js
@@ -0,0 +1,47 @@
+let d = new Date();
+var newDate = d.getMonth() + 1 + '.' + d.getDate() + '.' + d.getFullYear();
+//***********************
+//set the url and Api key 
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip=';
+const apiKey = 'e0bfdba6e59e1352a3b56a0ceebe673c';
+document.getElementById('generate').addEventListener('click', performAction);
+//function to make the information save and appear on the screen
+function performAction(e) {
+    const newzip = document.getElementById('zip').value;
+    getTemperature(baseURL, newzip, apiKey);
+}
+const getTemperature = async(baseURL, zip, key) => {
+    //Variable to compile, link and receive output
+    const res = await fetch(baseURL + zip + ",us&appid=" + key + "&units=metric");
+    try {
+        //to get the information
+        const data = await res.json();
+        const Temperature = data.main.temp;
+        const feel = document.getElementById("feelings").value
+        await fetch("/saveInformations", {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                date: newDate,
+                feelings: feel,
+                temp: Temperature
+            })
+        });
+        //to set a the input to the div and print it on the screen
+        const response1 = await fetch("/getInformations")
+        const fdata = await response1.json();
+        let ldate = document.getElementById("date");
+        let ltemp = document.getElementById("temp");
+        let lcontent = document.getElementById("content");
+        ldate.textContent = `date: ${fdata.date}`;
+        ltemp.textContent = "temp:" + fdata.temp;
+        lcontent.textContent = "feel:" + fdata.feelings;
+        //if any thing go wrong
+    } catch (error) {
+        console.log("error", error);
+        // appropriately handle the error
+    }
+}
